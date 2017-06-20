b9ea20c45e4da0086c1ae07e09ae1fd666590848
CaoPengfei <whiskeyfei@users.noreply.github.com>
2017-06-20 13:18:24 +0800

    enable ProGuard

--- lib.utils/src/main/java/com/library/utils/ToolsUtil.java
+++ lib.utils/src/main/java/com/library/utils/ToolsUtil.java
@@ -48,15 +48,15 @@ public class ToolsUtil {
     public static int getStatusHeight(Context context) {
 
         int statusHeight = -1;
-        try {
-            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
-            Object object = clazz.newInstance();
-            int height = Integer.parseInt(clazz.getField("status_bar_height")
-                    .get(object).toString());
-            statusHeight = context.getResources().getDimensionPixelSize(height);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+//            Object object = clazz.newInstance();
+//            int height = Integer.parseInt(clazz.getField("status_bar_height")
+//                    .get(object).toString());
+//            statusHeight = context.getResources().getDimensionPixelSize(height);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         return statusHeight;
     }
 
